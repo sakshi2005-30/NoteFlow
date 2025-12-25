c6166f835d424f9d37ab02de1b472d64d8460366
sakshi2005-30 <sakshikurda305@gmail.com>
2025-12-25 18:24:35 +0530

    added onsubmit correctly in notemodal

--- frontend/src/components/NoteModal.jsx
+++ frontend/src/components/NoteModal.jsx
@@ -4,7 +4,8 @@ const NoteModal = ({onClose,onSave,editNote,onEditSave}) => {
     const [title,setTitle]=useState("");
     const [description,setDescription]=useState("");
 
-    const handleSubmit=async()=>{
+    const handleSubmit=async(e)=>{
+      e.preventDefault();
        try{
         const token=localStorage.getItem("token");
         if(editNote){
@@ -44,8 +45,10 @@ const NoteModal = ({onClose,onSave,editNote,onEditSave}) => {
   return (
     <div className="fixed inset-0 bg-black/30 flex justify-center items-center z-50 ">
       <div className="bg-gray-900 p-10 w-full max-w-md rounded-xl shadow-md">
-        <h2 className="text-2xl font-semibold text-center mb-4">{editNote ?"Update Note":"Create Note"}</h2>
-        <form className="space-y-4">
+        <h2 className="text-2xl font-semibold text-center mb-4">
+          {editNote ? "Update Note" : "Create Note"}
+        </h2>
+        <form onSubmit={handleSubmit} className="space-y-4">
           <input
             type="text"
             value={title}
@@ -62,9 +65,8 @@ const NoteModal = ({onClose,onSave,editNote,onEditSave}) => {
             rows={5}
           />
           <div className="space-x-4">
-            <button className="px-4 py-2 bg-gray-300 text-black font-medium rounded-md hover:bg-gray-400 cursor-pointer"
-            onClick={handleSubmit}>
-              {editNote?"Update":"Create"}
+            <button className="px-4 py-2 bg-gray-300 text-black font-medium rounded-md hover:bg-gray-400 cursor-pointer">
+              {editNote ? "Update" : "Create"}
             </button>
             <button
               onClick={onClose}
